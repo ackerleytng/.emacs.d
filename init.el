920c4652afc3ce629c60cdefaf48d66b8262935e
Ackerley Tng <ackerleytng@gmail.com>
2023-03-06 22:20:52 -0800

    Bootstrap straight

--- init.el
+++ init.el
@@ -64,6 +64,23 @@
   :ensure t)
 
 ;;------------------------------------------------------------------------
+;; Bootstrap straight
+;;------------------------------------------------------------------------
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;------------------------------------------------------------------------
 ;; Settings
 ;;------------------------------------------------------------------------
 
