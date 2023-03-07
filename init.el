df70fb36db269214008f1fe34fa7c3fe01d7a31b
Ackerley Tng <ackerleytng@gmail.com>
2023-03-06 22:20:43 -0800

    Add markdown-mode

--- init.el
+++ init.el
@@ -333,6 +333,12 @@
   :init
   (marginalia-mode))
 
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init
+  (setq markdown-command "multimarkdown"))
+
 (use-package modus-themes
   :ensure t
   :init
